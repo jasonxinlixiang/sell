64cc70f1a178d1b8769d08cd17e7ca615511a26d
Jason Xiang <494312263@qq.com>
2018-06-12 21:54:39 +0800

    4-1
    Product Category dao-1

---
+++ src/main/java/com/imooc/repository/ProductCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.imooc.repository;
+
+import com.imooc.dataobject.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+}
