3b2d2257b5720e3ee1819db88be86709a5772fd0
Jason Xiang <494312263@qq.com>
2018-06-17 16:00:56 +0800

    5-4
    Buyer Product api-2

--- src/main/java/com/imooc/controller/BuyerProductController.java
+++ src/main/java/com/imooc/controller/BuyerProductController.java
@@ -3,11 +3,22 @@ package com.imooc.controller;
 import com.imooc.VO.ProductInfoVO;
 import com.imooc.VO.ProductVO;
 import com.imooc.VO.ResultVO;
+import com.imooc.dataobject.ProductCategory;
+import com.imooc.dataobject.ProductInfo;
+import com.imooc.service.CategoryService;
+import com.imooc.service.ProductService;
+import com.imooc.utils.ResultVOUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 * 买家商品
@@ -17,18 +28,48 @@ import java.util.Arrays;
 @RequestMapping("/buyer/product")
 public class BuyerProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/list")
     public ResultVO list() {
-        ResultVO resultVO = new ResultVO();
-        ProductVO productVO = new ProductVO();
-        ProductInfoVO productInfoVO = new ProductInfoVO();
+        //1. 查询所有的上架商品
+        List<ProductInfo> productInfoList = productService.findUpAll();
 
-        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
-        resultVO.setData(Arrays.asList(productVO));
-        resultVO.setCode(0);
-        resultVO.setMsg("success");
+        //2. 查询类目（一次性查询）
+        //List<Integer> categoryTypeList = new ArrayList<>();
+        //传统方法
+       /* for (ProductInfo productInfo : productInfoList) {
+            categoryTypeList.add(productInfo.getCategoryType());
+        }*/
+        //精简方法(java8, lambda)
+        List<Integer> categoryTypeList = productInfoList.stream().map(e -> e.getCategoryType()).collect(Collectors.toList());
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(categoryTypeList);
 
-        return resultVO;
+        //3. 数据拼装
+        List<ProductVO> productVOList = new ArrayList<>();
+        for (ProductCategory productCategory : productCategoryList) {
+            ProductVO productVO = new ProductVO();
+            productVO.setCategoryName(productCategory.getCategoryName());
+            productVO.setCategoryType(productCategory.getCategoryType());
+
+
+            List<ProductInfoVO> productInfoVOList = new ArrayList<>();
+            for (ProductInfo productInfo : productInfoList) {
+                if (productInfo.getCategoryType().equals(productCategory.getCategoryType())) {
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo, productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+            productVO.setProductInfoVOList(productInfoVOList);
+            productVOList.add(productVO);
+        }
+
+        return ResultVOUtil.success(productVOList);
     }
 
 }
