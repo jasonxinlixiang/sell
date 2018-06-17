943fb92026802d6b9f0d4c39111349ed0a316cfd
Jason Xiang <494312263@qq.com>
2018-06-17 15:01:34 +0800

    5-3
    Buyer Product api-1

---
+++ src/main/java/com/imooc/controller/BuyerProductController.java
@@ -0,0 +1,34 @@
+package com.imooc.controller;
+
+import com.imooc.VO.ProductInfoVO;
+import com.imooc.VO.ProductVO;
+import com.imooc.VO.ResultVO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+/*
+* 买家商品
+*
+*/
+@RestController
+@RequestMapping("/buyer/product")
+public class BuyerProductController {
+
+    @GetMapping("/list")
+    public ResultVO list() {
+        ResultVO resultVO = new ResultVO();
+        ProductVO productVO = new ProductVO();
+        ProductInfoVO productInfoVO = new ProductInfoVO();
+
+        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
+        resultVO.setData(Arrays.asList(productVO));
+        resultVO.setCode(0);
+        resultVO.setMsg("success");
+
+        return resultVO;
+    }
+
+}
