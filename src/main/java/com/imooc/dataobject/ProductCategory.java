9574ec1d778c06c8e9e8be8045361ca96ef1984d
Jason Xiang <494312263@qq.com>
2018-06-17 10:48:09 +0800

    4-2
    Product Category dao-2

--- src/main/java/com/imooc/dataobject/ProductCategory.java
+++ src/main/java/com/imooc/dataobject/ProductCategory.java
@@ -1,5 +1,7 @@
 package com.imooc.dataobject;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,6 +11,7 @@ import javax.persistence.Id;
 *
 */
 @Entity
+@Data
 public class ProductCategory {
 
     /* 类目id. */
@@ -22,36 +25,11 @@ public class ProductCategory {
     /* 类目编号. */
     private Integer categoryType;
 
-    public Integer getCategoryId() {
-        return categoryId;
+    public ProductCategory() {
     }
 
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
+    public ProductCategory(String categoryName, Integer categoryType) {
         this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
         this.categoryType = categoryType;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductCategory{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                ", categoryType=" + categoryType +
-                '}';
     }
 }
--- src/test/java/com/imooc/repository/ProductCategoryRepositoryTest.java
+++ src/test/java/com/imooc/repository/ProductCategoryRepositoryTest.java
@@ -1,11 +1,16 @@
 package com.imooc.repository;
 
 import com.imooc.dataobject.ProductCategory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,13 +26,18 @@ public class ProductCategoryRepositoryTest {
     }
 
     @Test
+    @Transactional
     public void saveTest() {
-        ProductCategory productCategory = new ProductCategory();
-        productCategory.setCategoryName("女生最爱");
-        productCategory.setCategoryType(3);
-        repository.save(productCategory);
+        ProductCategory productCategory = new ProductCategory("Jason最爱", 5);
+        ProductCategory result = repository.save(productCategory);
+        Assert.assertNotNull(result);
     }
 
-    
 
+    @Test
+    public void findByCategoryTypeInTest() {
+        List<Integer> list = Arrays.asList(2, 3, 4);
+        List<ProductCategory> result = repository.findByCategoryTypeIn(list);
+        Assert.assertNotEquals(0, result.size());
+    }
 }
