795aef348e34f34b1e32fd92c433654d2e3b1f49
Jason Xiang <494312263@qq.com>
2018-07-19 18:41:53 +0800

    8-2
    Create Wechat pay request

---
+++ src/main/java/com/imooc/service/PayService.java
@@ -0,0 +1,9 @@
+package com.imooc.service;
+
+import com.imooc.dto.OrderDTO;
+
+public interface PayService {
+
+	void create(OrderDTO orderDTO);
+
+}
---
+++ src/test/java/com/imooc/service/impl/PayServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.imooc.service.impl;
+
+import com.imooc.dto.OrderDTO;
+import com.imooc.service.OrderService;
+import com.imooc.service.PayService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class PayServiceImplTest {
+
+	@Autowired
+	private PayService payService;
+
+	@Autowired
+	private OrderService orderService;
+
+	@Test
+	public void create() {
+		OrderDTO orderDTO = this.orderService.findOne("");
+		payService.create(orderDTO);
+	}
+}
