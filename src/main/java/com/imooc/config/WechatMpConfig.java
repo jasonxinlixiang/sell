4e99ea065a3426d4299c9213aa2caff842f78280
Jason Xiang <494312263@qq.com>
2018-09-24 22:15:08 +0800

    12-9
    Seller Info - Wechat template message push

--- src/main/java/com/imooc/config/WechatMpConfig.java
+++ src/main/java/com/imooc/config/WechatMpConfig.java
@@ -24,7 +24,7 @@ public class WechatMpConfig {
     @Bean
     public WxMpConfigStorage wxMpConfigStorage() {
         WxMpInMemoryConfigStorage wxMpConfigStorage = new WxMpInMemoryConfigStorage();
-        wxMpConfigStorage.setAppId(accountConfig.getMpAppPayId());
+        wxMpConfigStorage.setAppId(accountConfig.getMpAppId());
         wxMpConfigStorage.setSecret(accountConfig.getMpAppSecret());
         return wxMpConfigStorage;
     }
