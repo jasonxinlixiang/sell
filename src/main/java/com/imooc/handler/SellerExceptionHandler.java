09163668cfc7022cf823447c6d027dba6a1bf387
Jason Xiang <494312263@qq.com>
2018-10-07 14:26:04 +0800

    13-1
    Exception Handler

--- src/main/java/com/imooc/handler/SellerExceptionHandler.java
+++ src/main/java/com/imooc/handler/SellerExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.imooc.handler;
 
+import com.imooc.VO.ResultVO;
 import com.imooc.config.ProjectUrl;
+import com.imooc.exception.SellException;
 import com.imooc.exception.SellerAuthorizeException;
+import com.imooc.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
@@ -20,4 +26,12 @@ public class SellerExceptionHandler {
                 "oTgZpwcDx4xfGs9XtQeMmN0miMbw&response_type=code&scope=snsapi_login&state=http://demosell.natapp1.cc/sell/wechat/qrUserInfo";
         return new ModelAndView("redirect:" + returnUrl);
     }
+
+    @ExceptionHandler(value = SellException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResultVO handlerSellerException(SellException e) {
+        return ResultVOUtil.error(e.getCode(), e.getMessage());
+    }
+
 }
